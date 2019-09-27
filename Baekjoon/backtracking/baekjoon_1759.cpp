2de77f1637a347b76c378bca7179840d8c221f70
SEUNGHO <seung7642@gmail.com>
2019-09-27 16:03:59 +0900

    암호 만들기

--- Baekjoon/backtracking/baekjoon_1759.cpp
+++ Baekjoon/backtracking/baekjoon_1759.cpp
@@ -9,42 +9,40 @@ char input[MAX];
 bool visited[MAX];
 
 void dfs(int node, int depth, int n, int m) { // n은 모음, m은 자음
-	if (depth == L) {
-		if (n < 1 || m < 2) return;
-		for (int i = 0; i < C; i++) {
-			if (visited[i]) cout << input[i];
-		}
-		cout << "\n";
-		return;
-	}
+    if (depth == L) {
+        if (n < 1 || m < 2) return;
+        for (int i = 0; i < C; i++) {
+            if (visited[i]) cout << input[i];
+        }
+        cout << "\n";
+        return;
+    }
 
-	for (int i = node; i < C; i++) {
-		if (!visited[i]) {
-			visited[i] = true;
-			int nextN = n;
-			int nextM = m;
+    for (int i = node; i < C; i++) {
+        visited[i] = true;
+        int nextN = n;
+        int nextM = m;
 
-			if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u')
-				nextN += 1;
-			else
-				nextM += 1;
+        if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u')
+            nextN += 1;
+        else
+            nextM += 1;
 
-			dfs(i + 1, depth + 1, nextN, nextM);
-			visited[i] = false;
-		}
-	}
+        dfs(i + 1, depth + 1, nextN, nextM);
+        visited[i] = false;
+    }
 }
 
 int main(void) {
-	cin >> L >> C;
+    cin >> L >> C;
 
-	for (int i = 0; i < C; i++) {
-		cin >> input[i];
-	}
+    for (int i = 0; i < C; i++) {
+        cin >> input[i];
+    }
 
-	sort(input, input + C);
+    sort(input, input + C);
 
-	dfs(0, 0, 0, 0);
+    dfs(0, 0, 0, 0);
 
-	return 0;
+    return 0;
 }
