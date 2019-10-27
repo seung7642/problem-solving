dc114c17da22664b0e42227fd6c0a91aeec7a621
SEUNGHO <seung7642@gmail.com>
2019-10-27 23:56:39 +0900

    라인 3번

--- 2019CodingTest/LINE/line3.cpp
+++ 2019CodingTest/LINE/line3.cpp
@@ -4,35 +4,35 @@
 using namespace std;
 
 bool cmp(pair<int, int> a, pair<int, int> b) {
-	if (a.second == b.second)
-		return a.first < b.first;
-	else
-		return a.second < b.second;
+    if (a.second == b.second)
+        return a.first < b.first;
+    else
+        return a.second < b.second;
 }
 
 int main(void) {
-	int n;
-	cin >> n;
+    int n;
+    cin >> n;
 
-	vector<pair<int, int>> v(n);
-	for (int i = 0; i < n; i++) {
-		cin >> v[i].first >> v[i].second;
-	}
-	sort(v.begin(), v.end(), cmp);
+    vector<pair<int, int>> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i].first >> v[i].second;
+    }
+    sort(v.begin(), v.end(), cmp);
 
-	int toiletCount = 0;
-	for (int i = 0; i < n; i++) {
-		int cnt = 1;
-		for (int j = i + 1; j < n; j++) {
-			if (v[i].first <= v[j].first && v[i].second > v[j].first) {
-				cnt++;
-			}
-		}
-		if (toiletCount < cnt)
-			toiletCount = cnt;
-	}
+    int toiletCount = 0;
+    for (int i = 0; i < n; i++) {
+        int cnt = 1;
+        for (int j = i + 1; j < n; j++) {
+            if (v[i].first <= v[j].first && v[i].second > v[j].first) {
+                cnt++;
+            }
+        }
+        if (toiletCount < cnt)
+            toiletCount = cnt;
+    }
 
-	cout << toiletCount << "\n";
+    cout << toiletCount << "\n";
 
-	return 0;
+    return 0;
 }
