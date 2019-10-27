c1b1ec150bbb96f8c0ae0594190d14cd2045d4f5
SEUNGHO <seung7642@gmail.com>
2019-10-28 00:09:45 +0900

    네이버 1번

--- 2019CodingTest/NAVER/naver1.cpp
+++ 2019CodingTest/NAVER/naver1.cpp
@@ -7,52 +7,52 @@ stack<string> temporaryLocker; // 임시 보관함
 stack<string> permanentLocker; // 영구 보관함
 
 vector<string> solution(vector<string> record) {
-	vector<string> ans;
+    vector<string> ans;
 
-	for (int i = 0; i < record.size(); i++) {
-		string command;
+    for (int i = 0; i < record.size(); i++) {
+        string command;
 
-		for (int j = 0; j < record[i].size(); j++)
-			command += record[i][j];
+        for (int j = 0; j < record[i].size(); j++)
+            command += record[i][j];
 
-		// 명령어에 따른 처리
-		if (command[0] == 'R') {
-			string emailAddress;
-			for (int j = 8; j < command.length(); j++) {
-				emailAddress += command[j];
-			}
-			temporaryLocker.push(emailAddress);
-		}
-		else if (command == "DELETE") {
-			if (!temporaryLocker.empty())
-				temporaryLocker.pop();
-		}
-		else if (command == "SAVE") {
-			// 임시 보관함 -> 영구 보관함으로 이동
-			stack<string> tmp;
-			int size = temporaryLocker.size();
-			for (int j = 0; j < size; j++) {
-				tmp.push(temporaryLocker.top());
-				temporaryLocker.pop();
-			}
+        // 명령어에 따른 처리
+        if (command[0] == 'R') {
+            string emailAddress;
+            for (int j = 8; j < command.length(); j++) {
+                emailAddress += command[j];
+            }
+            temporaryLocker.push(emailAddress);
+        }
+        else if (command == "DELETE") {
+            if (!temporaryLocker.empty())
+                temporaryLocker.pop();
+        }
+        else if (command == "SAVE") {
+            // 임시 보관함 -> 영구 보관함으로 이동
+            stack<string> tmp;
+            int size = temporaryLocker.size();
+            for (int j = 0; j < size; j++) {
+                tmp.push(temporaryLocker.top());
+                temporaryLocker.pop();
+            }
 
-			for (int j = 0; j < size; j++) {
-				permanentLocker.push(tmp.top());
-				tmp.pop();
-			}
-		}
-	}
+            for (int j = 0; j < size; j++) {
+                permanentLocker.push(tmp.top());
+                tmp.pop();
+            }
+        }
+    }
 
-	vector<string> tmp;
-	int permanentSize = permanentLocker.size();
-	for (int i = 0; i < permanentSize; i++) {
-		tmp.push_back(permanentLocker.top());
-		permanentLocker.pop();
-	}
+    vector<string> tmp;
+    int permanentSize = permanentLocker.size();
+    for (int i = 0; i < permanentSize; i++) {
+        tmp.push_back(permanentLocker.top());
+        permanentLocker.pop();
+    }
 
-	for (int i = tmp.size() - 1; i >= 0; i--) {
-		ans.push_back(tmp[i]);
-	}
+    for (int i = tmp.size() - 1; i >= 0; i--) {
+        ans.push_back(tmp[i]);
+    }
 
-	return ans;
+    return ans;
 }
