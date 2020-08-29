e18267812129da543baf0f77a63007fc8bc87af9
Seungho Lee <seung7642@gmail.com>
2020-08-29 21:40:30 +0900

    최소 비용 신장 트리 (크루스칼 알고리즘)

---
+++ practice-algorithm/src/main/java/graph/minimumSpanningTree/BOJ_1197.java
@@ -0,0 +1,88 @@
+package graph.minimumSpanningTree;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+// 문제: 최소 스패닝 트리
+public class BOJ_1197 {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static int V, E;
+    private static List<Edge> list;
+    private static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        list = new ArrayList<>();
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            list.add(new Edge(u, v, weight));
+        }
+
+        parent = new int[V + 1];
+        for (int i = 1; i < V + 1; i++)
+            parent[i] = i;
+
+        Collections.sort(list);
+
+        int sum = 0;
+        for (Edge edge : list) {
+            if (!isSameParent(edge.u, edge.v)) {
+                sum += edge.weight;
+                union(edge.u, edge.v);
+            }
+        }
+
+        bw.write(sum + "\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) parent[y] = x;
+    }
+
+    private static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    private static boolean isSameParent(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) return true;
+        else return false;
+    }
+}
+
+class Edge implements Comparable<Edge> {
+    int u, v, weight;
+
+    public Edge(int u, int v, int weight) {
+        this.u = u;
+        this.v = v;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        if (weight < o.weight) return -1;
+        else if (weight == o.weight) return 0;
+        else return 1;
+    }
+}
