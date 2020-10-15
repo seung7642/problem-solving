35b072fd56b6ba4de8dce1a2e44fc3c90e5689df
Seungho Lee <seung7642@gmail.com>
2020-10-15 19:49:55 +0900

    경로 찾기 - 플로이드 와샬 기본 문제

---
+++ practice-algorithm/src/main/java/graph/shortestPath/BOJ_11403.java
@@ -0,0 +1,51 @@
+package graph.shortestPath;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11403 {
+
+    private static final int INF = 1000000000;
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int N;
+    private static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        floydWarshall();
+        print();
+        br.close();
+    }
+
+    private static void floydWarshall() {
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (arr[i][j] == 1 || (arr[i][k] + arr[k][j] >= 1))
+                        arr[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    private static void print() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
