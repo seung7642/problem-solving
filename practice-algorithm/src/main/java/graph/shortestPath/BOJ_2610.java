e66c5d6cf5b88e3d05fb3a1b1221ff988d3344d1
Seungho Lee <seung7642@gmail.com>
2020-10-18 12:08:03 +0900

    회의준비 - 플로이드 와샬 + DFS (그룹지은 후 각 그룹별로 대표 선정)

---
+++ practice-algorithm/src/main/java/graph/shortestPath/BOJ_2610.java
@@ -0,0 +1,107 @@
+package graph.shortestPath;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_2610 {
+
+    private static final int INF = 1000000000;
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int[][] dist;
+    private static int N, M, groupNumber;
+    private static List<List<Integer>> com;
+    private static int[] check;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        check = new int[N + 1];
+        dist = new int[N + 1][N + 1];
+        com = new ArrayList<>();
+        for (int i = 1; i <= N; i++)
+            com.add(new ArrayList<>());
+
+        init();
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            dist[x][y] = dist[y][x] = 1;
+        }
+
+        floydWarshall();
+
+        for (int i = 1; i <= N; i++) {
+            if (check[i] != 0) continue;
+            groupNumber++;
+            dfs(i);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 1; i <= groupNumber; i++) {
+            pq.add(getGroupRepresentation(i));
+        }
+
+        System.out.println(groupNumber);
+        while (!pq.isEmpty()) {
+            System.out.println(pq.poll());
+        }
+
+        br.close();
+    }
+
+    private static int getGroupRepresentation(int groupNumber) {
+        List<Integer> groupMember = com.get(groupNumber);
+        int min_sum = 100007, sum, representation = groupMember.get(0);
+
+        for (int i = 0; i < groupMember.size(); i++) {
+            sum = 0;
+            int x = groupMember.get(i);
+
+            for (int j = 0; j < groupMember.size(); j++) {
+                int y = groupMember.get(j);
+                if (dist[x][y] == INF) continue;
+                sum = Math.max(sum, dist[x][y]);
+            }
+            if (min_sum > sum) {
+                min_sum = sum;
+                representation = x;
+            }
+        }
+        return representation;
+    }
+
+    private static void floydWarshall() {
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (dist[i][k] == INF || dist[k][j] == INF || i == j) continue;
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+
+    private static void dfs(int start) {
+        if (check[start] != 0) return;
+        check[start] = groupNumber;
+        com.get(groupNumber).add(start);
+
+        for (int i = 1; i <= N; i++) {
+            if (dist[start][i] == 0 || dist[start][i] == INF) continue;
+            dfs(i);
+        }
+    }
+
+    private static void init() {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) continue;
+                dist[i][j] = INF;
+            }
+        }
+    }
+}
