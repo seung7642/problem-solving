daf9a6cd53410504568ceded4e89a3be0119ce4b
Seungho Lee <seung7642@gmail.com>
2020-10-23 18:19:02 +0900

    미로만들기 - 우선순위 큐를 활용한 BFS (한 번 봐두기 !)

---
+++ practice-algorithm/src/main/java/graph/shortestPath/BOJ_2665.java
@@ -0,0 +1,80 @@
+package graph.shortestPath;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class BOJ_2665 {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int N;
+    private static int[][] map;
+    private static boolean[][] visited;
+    private static int[] dx = {-1, 1, 0, 0};
+    private static int[] dy = {0, 0, 1, -1};
+
+    static class Node implements Comparable<Node> {
+
+        int x, y, cnt;
+
+        public Node(int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return (cnt <= o.cnt ? -1 : 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        init();
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Character.getNumericValue(str.charAt(j));
+            }
+        }
+
+        System.out.println( bfs(0, 0) );
+        br.close();
+    }
+
+    private static int bfs(int x, int y) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(x, y, 0));
+
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+
+            if (cur.x == N - 1 && cur.y == N - 1) return cur.cnt;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+                if (visited[ny][nx]) continue;
+                visited[ny][nx] = true;
+
+                if (map[ny][nx] == 0) { // 검은 방
+                    pq.add(new Node(nx, ny, cur.cnt + 1));
+                } else {
+                    pq.add(new Node(nx, ny, cur.cnt));
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static void init() {
+        map = new int[N][N];
+        visited = new boolean[N][N];
+    }
+}
