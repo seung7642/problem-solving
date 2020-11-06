76fe82c38f1cfe1226234b067956eb20bc9afd9e
Seungho Lee <seung7642@gmail.com>
2020-11-06 20:25:35 +0900

    N과 M (5) - 백트래킹 (기본유형)

---
+++ practice-algorithm/src/main/java/graph/backtracking/BOJ_15654.java
@@ -0,0 +1,57 @@
+package graph.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_15654 {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int N, M;
+    private static int[] arr;
+    private static int[] ans;
+    private static boolean[] visited;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        ans = new int[M];
+        visited = new boolean[N];
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        dfs(0);
+
+        System.out.println(sb);
+        br.close();
+    }
+
+    private static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(ans[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (visited[i]) continue;
+
+            visited[i] = true;
+            ans[depth] = arr[i];
+            dfs(depth + 1);
+            visited[i] = false;
+        }
+    }
+}
