f1754ffa80942fc96f9868147f9229bae0b0d8ea
Seungho Lee <seung7642@gmail.com>
2020-11-22 21:57:46 +0900

    주사위 윷놀이 - 백트래킹 + 시뮬레이션 (어려움, 유형 익히기 !)

--- practice-algorithm/src/main/java/graph/backtracking/BOJ_17825.java
+++ practice-algorithm/src/main/java/graph/backtracking/BOJ_17825.java
@@ -14,30 +14,31 @@ public class BOJ_17825 {
     private static Node start; // 시작지점
     private static int answer = Integer.MIN_VALUE;
 
-    static class Node { // 윷놀이 판을 연결 리스트 구조에 담는다.
-        int val;
-        boolean isEmpty, isFinish;
-        Node next, fastPath;
+    static class Node {   // 윷놀이 판을 연결 리스트 구조에 담는다.
+        int score;        // 윷놀이판에서 해당 칸의 점수.
+        boolean isEmpty;  // 해당 칸이 비었는지. (다른 말이 와있는 칸인지 체크)
+        boolean isFinish; // 도착 지점인지를 체크.
+        Node next;
+        Node fastPath;    // 윷놀이판에서 10, 20, 30은 두 가지 방향이 존재한다.
 
-        Node(int val) {
-            this.val = val;
+        public Node(int score) {
+            this.score = score;
             this.isEmpty = true;
         }
 
         // 노드 연결 (연결 리스트 구조)
-        Node addNext(int value) {
-            Node nextNode = new Node(value);
+        public Node addNext(int score) {
+            Node nextNode = new Node(score);
             this.next = nextNode;
             return nextNode;
         }
 
         // 노드 찾기 (지름길 놓는 지점을 찾기 위한 함수)
-        static Node getNode(Node start, int target) {
-            // 시작 지점부터 탐색해가며 특정 노드를 찾습니다.
+        public static Node getNode(Node start, int target) {
             Node temp = start;
-            while (true) {
+            while (true) { // 시작 지점부터 탐색해가며 특정 노드를 찾습니다.
                 if (temp == null) return null;
-                if (temp.val == target) return temp;
+                if (temp.score == target) return temp;
                 temp = temp.next;
             }
         }
@@ -71,87 +72,72 @@ public class BOJ_17825 {
     }
 
     private static int gameStart() {
-        // 말들을 시작지점으로 배치
-        Arrays.fill(horse, start);
-        int score = 0;
+        Arrays.fill(horse, start); // 말들을 시작 지점으로 배치
 
+        int score = 0;
         for (int i = 1; i <= 10; i++) {
             Node cur = horse[order[i]]; // 순열로 할당된 순서대로 말들을 움직입니다.
             cur.isEmpty = true; // 현재 있는 칸을 비워줍니다.
 
-            // 주사위에 나온 수치만큼 이동합니다.
-            for (int j = 1; j <= dice[i]; j++) {
-                // 말이 시작하는 시점이면서 지름길(파란색)로 가는 길이 있다면
-                if (j == 1 && cur.fastPath != null) {
+            for (int j = 1; j <= dice[i]; j++) { // 주사위에 나온 수치만큼 이동합니다.
+                if (j == 1 && cur.fastPath != null) { // 처음 이동을 시작하려는 위치가 파란색 칸이라면.
                     cur = cur.fastPath; // 지름길로 이동(파란색 방향)
                 } else {
                     cur = cur.next; // 빨간색 방향으로 이동
                 }
             }
 
-            // 이동 후, 말  위치 설정
-            horse[order[i]] = cur;
+            horse[order[i]] = cur; // 이동 후, 말 위치 설정
 
-            // 마지막 노드에 도착하지 않았으며 이미 말이 있는 노드라면
-            if (!cur.isEmpty && !cur.isFinish) {
-                // 주사위에 할당받은 말들의 순서가 무효처리.
-                score = 0;
+            if (!cur.isEmpty && !cur.isFinish) { // 이동을 마친 칸에 다른 말이 있다면, 해당 말은 고를 수 없다.
+                score = 0; // 주사위에 할당받은 말들의 순서가 무효처리.
                 break;
             } else {
-                // 말이 존재하는 것으로 표시
-                cur.isEmpty = false;
-                // 점수 추가
-                score += cur.val;
+                cur.isEmpty = false; // 말이 존재하는 것으로 표시
+                score += cur.score; // 해당 칸의 점수 추가
             }
         } // 게임종료
 
         // 다음 게임을 위해 말들의 위치를 지워줍니다.
-        for (int i = 1; i <= 4; i++) {
+        for (int i = 1; i <= 4; i++)
             horse[i].isEmpty = true;
-        }
-        // 획든한 점수 반환
-        return score;
+
+        return score; // 획든한 점수 반환
     }
 
     private static void init() {
-        // 시작 위치와 점수
-        start = new Node(0);
+        start = new Node(0); // 시작 지점. (시작 지점의 스코어는 0)
 
         Node temp = start;
         for (int i = 2; i <= 40; i += 2) {
-            // 바깥쪽 경로 설정
-            temp = temp.addNext(i);
+            temp = temp.addNext(i); // 윷놀이판 바깥쪽 경로 설정
         }
 
-        // 도착 지점
-        Node end = temp.addNext(0);
+        Node end = temp.addNext(0); // 도착 지점. (도착 지점의 스코어는 0)
         end.isFinish = true;
-        // 자기자신을 가르키도록 설정
-        // 도착 지점을 넘어서 이동하여 NPE 방지
-        end.next = end;
+        end.next = end; // 자기자신을 가르키도록 설정 (도착 지점을 넘어서는 이동에 대해 NPE 방지)
 
-        // 가운데 교차점 (val = 25)
-        Node crossroads = new Node(25);
+        Node crossroads = new Node(25); // 가운데 교차점 (score = 25)
 
-        // 교차점(25) - 30 - 35 - 40
+        // 교차점(25) -> 30 -> 35 -> 40
         temp = crossroads.addNext(30);
         temp = temp.addNext(35);
         temp.next = Node.getNode(start, 40);
 
-        // 10 - 13 - 16 - 19 - 25(교차점)
+        // 10 -> 13 -> 16 -> 19 -> 25(교차점)
         temp = Node.getNode(start, 10);
         temp = temp.fastPath = new Node(13);
         temp = temp.addNext(16);
         temp = temp.addNext(19);
         temp.next = crossroads;
 
-        // 20 - 22 - 24 - 25(교차점)
+        // 20 -> 22 -> 24 -> 25(교차점)
         temp = Node.getNode(start, 20);
         temp = temp.fastPath = new Node(22);
         temp = temp.addNext(24);
         temp.next = crossroads;
 
-        // 30 - 28 - 27 - 26 - 25(교차점)
+        // 30 -> 28 -> 27 -> 26 -> 25(교차점)
         temp = Node.getNode(start, 30);
         temp = temp.fastPath = new Node(28);
         temp = temp.addNext(27);
