3221d75c6ab113bf3e8cae50b93d68bca50785fa
Seungho Lee <seung7642@gmail.com>
2021-03-18 14:41:25 +0900

    문서 검색 - 문자열 매칭 KMP 알고리즘 (기본유형)

---
+++ practice-algorithm/src/main/java/string/kmp/BOJ_1543.java
@@ -0,0 +1,51 @@
+package string.kmp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_1543 {
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String origin = br.readLine();
+            String pattern = br.readLine();
+            int ans = kmp(origin, pattern);
+            System.out.println(ans);
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
+    }
+
+    private static int[] getPi(String pattern) {
+        int[] pi = new int[pattern.length()];
+        int j = 0;
+        for (int i = 1; i < pattern.length(); i++) {
+            while (j > 0 && pattern.charAt(i) != pattern.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (pattern.charAt(i) == pattern.charAt(j)) {
+                pi[i] = ++j;
+            }
+        }
+        return pi;
+    }
+
+    private static int kmp(String origin, String pattern) {
+        int[] pi = getPi(pattern);
+        int j = 0, cnt = 0;
+        for (int i = 0; i < origin.length(); i++) {
+            while (j > 0 && origin.charAt(i) != pattern.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (origin.charAt(i) == pattern.charAt(j)) {
+                if (j == pattern.length() - 1) {
+                    cnt++;
+                    j = 0;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
