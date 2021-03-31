6ac36a239d2258ea8ea0d58cb0cee7866cf825f8
Seungho Lee <seung7642@gmail.com>
2021-04-01 00:14:48 +0900

    구간 합 구하기 - 세그먼트 트리, 펜윅 트리 두 가지 방법 사용해보기.

--- Baekjoon/src/main/java/dataStructure/segmentTree/BOJ_2042.java
+++ Baekjoon/src/main/java/dataStructure/segmentTree/BOJ_2042.java
@@ -7,38 +7,25 @@ import java.util.StringTokenizer;
 
 public class BOJ_2042 {
 
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static int N, M, K;
-    private static long[] arr, tree;
+    private static long[] arr;
+    private static SegmentTree tree;
+    private static FenwickTree tree2;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
         arr = new long[N + 1];
-        for (int i = 1; i <= N; i++)
+        for (int i = 1; i <= N; i++) {
             arr[i] = Long.parseLong(br.readLine());
+        }
 
-        // 2^k >= N를 만족하는 최소의 k를 찾아야 한다.
-        // 양변에 log를 취하면,
-        // k >= logN / log2
-        // (logN / log2)의 값을 올림한 후 1을 더하면 k가 됨.
-        // 위에서 구한 k를 제곱하면 세그먼트 트리의 size를 구할 수 있다.
-
-        // int k = (int) Math.ceil(Math.log(N) / Math.log(2)) + 1;
-        // int size = (int) Math.pow(2, k);
-
-        // tree = new long[size];
-
-        // 세그먼트 트리의 사이즈를 구하는 위의 과정이 귀찮다면, N에 4를 곱해도 무방. (이건 왜지?)
-        tree = new long[N * 4];
-
-        // 루트 노드는 0번이 아니라 1번부터 시작한다.
-        // 그래야 자기 자식 노드를 표현하기 편하기 때문.
-        // 루트 노드가 1이면 왼쪽 자식은 1*2, 오른쪽 자식은 1*2+1
-        init(1, N, 1);
+//        tree = new SegmentTree(arr);
+        tree2 = new FenwickTree(arr);
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M + K; i++) {
@@ -50,9 +37,12 @@ public class BOJ_2042 {
             if (a == 1) { // b번 째 수를 c로 수정한다.
                 long diff = c - arr[b]; // 기존의 값과 변경할 값의 차이
                 arr[b] = c;
-                update(1, N, 1, b, diff);
+//                tree.update(1, N, 1, b, diff);
+                tree2.update(b, diff);
             } else if (a == 2) { // b번째 수부터 c번째 수까지의 합을 출력한다.
-                sb.append(sum(1, N, 1, b, (int) c)).append("\n");
+//                long result = tree.query(1, N, 1, b, (int) c);
+                long result = tree2.query(b, (int) c);
+                sb.append(result).append("\n");
             }
         }
 
@@ -60,50 +50,73 @@ public class BOJ_2042 {
         br.close();
     }
 
-    // node가 담당하고 있는 구간 : [start, end]
-    private static long init(int start, int end, int node) {
-        if (start == end) { // 시작과 끝이 같다는건 리프 노드라는 의미.
-            return tree[node] = arr[start];
+    private static class SegmentTree {
+
+        long[] nums;
+        long[] tree;
+
+        public SegmentTree(long[] nums) {
+            this.nums = nums;
+            int k = (int) Math.ceil(Math.log(N) / Math.log(2));
+            int height = k + 1;
+            int size = (int) Math.pow(2, height);
+            tree = new long[size];
+            init(1, N, 1);
         }
 
-        int mid = (start + end) / 2;
-
-        // 재귀적으로 두 부분으로 나눈 뒤에 그 합을 자기 자신의 값으로 셋팅한다.
-        return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
-    }
-
-    // node가 담당하고 있는 구간 : [start, end]
-    // [left, right]: 합을 구해야하는 구간
-    private static long sum(int start, int end, int node, int left, int right) {
-        // 구하고자하는 합의 구간이 해당 노드가 가지는 합의 범위를 완전히 벗어난다면, 더이상 탐색할 필요가 없다.
-        if (left > end || right < start) {
-            return 0;
-        }
-
-        // 해당 노드가 가지는 합의 범위가 구하고자하는 합의 범위에 포함된다면, 더이상의 탐색이 무의미하다.
-        if (left <= start && end <= right) {
-            return tree[node];
-        }
-
-        // 자식 노드로 범위를 좁혀가며 탐색을 진행한다.
-        int mid = (start + end) / 2;
-        return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
-    }
-
-    // idx: 구간 합을 수정하고자 하는 노드
-    // diff: 수정할 값
-    private static void update(int start, int end, int node, int idx, long diff) {
-        // 변경하고자하는 인덱스가 해당 노드의 범위에 포함되지 않는다면 더이상 탐색이 필요없다.
-        if (idx < start || idx > end) {
-            return;
-        }
-
-        // 범위 안에 있으면 내려가며 다른 원소도 갱신
-        tree[node] += diff;
-        if (start != end) {
+        public long init(int start, int end, int node) {
+            if (start == end) return tree[node] = nums[start];
             int mid = (start + end) / 2;
-            update(start, mid, node * 2, idx, diff);
-            update(mid + 1, end, node * 2 + 1, idx, diff);
+            return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
+        }
+
+        public void update(int start, int end, int node, int idx, long diff) {
+            if (idx < start || idx > end) return;
+            tree[node] += diff;
+            if (start != end) {
+                int mid = (start + end) / 2;
+                update(start, mid, node * 2, idx, diff);
+                update(mid + 1, end, node * 2 + 1, idx, diff);
+            }
+        }
+
+        public long query(int start, int end, int node, int left, int right) {
+            if (left > end || right < start) return 0;
+            if (left <= start && end <= right) return tree[node];
+            int mid = (start + end) / 2;
+            return query(start, mid, node * 2, left, right) + query(mid + 1, end, node * 2 + 1, left, right);
+        }
+    }
+
+    private static class FenwickTree {
+
+        long[] tree;
+
+        public FenwickTree(long[] nums) {
+            tree = new long[N + 1];
+            for (int i = 1; i <= N; i++) {
+                update(i, nums[i]);
+            }
+        }
+
+        public void update(int idx, long diff) {
+            while (idx < tree.length) {
+                tree[idx] += diff;
+                idx += (idx & -idx);
+            }
+        }
+
+        public long query(int left, int right) {
+            return query(right) - query(left - 1);
+        }
+
+        public long query(int idx) {
+            long result = 0;
+            while (idx > 0) {
+                result += tree[idx];
+                idx &= idx - 1;
+            }
+            return result;
         }
     }
 }
