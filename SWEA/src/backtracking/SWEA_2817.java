5602ec614a29982466c7a0b1fa45c565e75314c5
Seungho Lee <seung7642@gmail.com>
2021-07-30 13:25:55 +0900

    부분 수열의 합 - 백트래킹 (기본편)

---
+++ SWEA/src/backtracking/SWEA_2817.java
@@ -0,0 +1,50 @@
+package backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 부분 수열의 합 (백트래킹의 대표 문제 중 하나)
+public class SWEA_2817 {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int T, N, K, cnt;
+    private static int[] arr;
+    private static boolean[] visited;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            arr = new int[N];
+            visited = new boolean[N];
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            cnt = 0;
+            backtracking(0, 0);
+            sb.append("#").append(tc).append(" ").append(cnt).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void backtracking(int sum, int depth) {
+        if (depth == N) {
+            if (sum == K) cnt++;
+            return;
+        }
+
+        if (visited[depth]) return;
+        visited[depth] = true;
+        backtracking(sum + arr[depth], depth + 1);
+        backtracking(sum, depth + 1);
+        visited[depth] = false;
+    }
+}
