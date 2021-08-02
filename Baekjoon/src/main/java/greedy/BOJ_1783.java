3f1bcac617a328d8ce2ee86a8e9f2dddd87b4ffc
Seungho Lee <seung7642@gmail.com>
2021-08-02 22:08:28 +0900

    병든 나이트 - 그리디 (기본편)

---
+++ Baekjoon/src/main/java/greedy/BOJ_1783.java
@@ -0,0 +1,37 @@
+package greedy;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1783 {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int result = 0;
+
+        if (N == 1) {
+            // N이 1이면 이동 불가 (시작지점만)
+            result = 1;
+        } else if (N == 2) {
+            // N이 2일 떈, 2번,3번 방향으로만 움직일 수 있음
+            // 절대 4방향 다 움직일 수 없어서 최댓값은 4
+            result = Math.min((M + 1) / 2, 4);
+        } else if (N >= 3) {
+            // M=7 부터 4방향 다 이동 가능
+            // 4방향 다 이동한 후에는 y값이 1씩 증가하는 1번,4번 이동을 반복
+            // 즉, M-2개의 칸을 갈 수 있음
+            if (M < 7) {
+                result = Math.min(M, 4);
+            } else {
+                result = M - 2;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
