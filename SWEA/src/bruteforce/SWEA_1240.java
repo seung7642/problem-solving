e6ddc2bfda5309d370e21fc228c99af2760ec431
Seungho Lee <seung7642@gmail.com>
2021-08-05 00:29:41 +0900

    단순 2진 암호코드 - 주어진 문자열에서 암호코드 찾기

--- SWEA/src/bruteforce/SWEA_1240.java
+++ SWEA/src/bruteforce/SWEA_1240.java
@@ -9,52 +9,72 @@ public class SWEA_1240 {
 
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static int T, N, M;
+    private static String[] rates = {"3211", "2221", "2122", "1411", "1132"
+            , "1231", "1114", "1312", "1213", "3112"}; // 각 비율을 저장한다. ex) "3211" --> 3:2:1:1 (가장 우측이 1로 시작.)
+    private static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        T = Integer.parseInt(st.nextToken());
-        String str, tmp, sub = "";
-        int[] decimalNum = new int[8];
-        String[] code = { "0001101", "0011001", "0010011", "0111101", "0100011"
-                , "0110001", "0101111", "0111011", "0110111", "0001011" };
-
+        T = Integer.parseInt(br.readLine());
         for (int tc = 1; tc <= T; tc++) {
-            int count = 0;
-            boolean isFind = false;
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
 
+            String subStr = "";
+            boolean isFind = false;
             for (int i = 0; i < N; i++) {
-                str = br.readLine();
+                String str = br.readLine();
                 for (int j = M - 1; j >= 0 && !isFind; j--) {
                     if (str.charAt(j) == '1') {
-                        sub = str.substring(j - 55, j + 1);
+                        subStr = str.substring(j - 55, j + 1);
                         isFind = true;
                     }
                 }
             }
 
-            for (int i = 0; i < sub.length(); i += 7) {
-                tmp = sub.substring(i, i + 7);
-                for (int j = 0; j < code.length; j++) {
-                    if (tmp.equals(code[j])) {
-                        decimalNum[count++] = j;
+            String[] codes = getCodeArray(1);
+            int[] resultArr = new int[10];
+            for (int i = 0, idx = 0; i < subStr.length(); i += 7) {
+                String compareStr = subStr.substring(i, i + 7);
+                for (int j = 0; j < codes.length; j++) {
+                    if (compareStr.equals(codes[j])) {
+                        resultArr[idx++] = j;
                     }
                 }
             }
 
-            int answer = (decimalNum[0] + decimalNum[2] + decimalNum[4] + decimalNum[6]) * 3
-                    + decimalNum[1] + decimalNum[3] + decimalNum[5] + decimalNum[7];
-            if (answer % 10 == 0) {
-                answer = 0;
-                for (int i = 0; i < decimalNum.length; i++) {
-                    answer += decimalNum[i];
+            int checkVal = (resultArr[0] + resultArr[2] + resultArr[4] + resultArr[6]) * 3
+                    + resultArr[1] + resultArr[3] + resultArr[5] + resultArr[7];
+
+            int ans = 0;
+            if (checkVal % 10 == 0) { // 최종 값이 10의 배수라면 정상적인 암호코드.
+                for (int num : resultArr) {
+                    ans += num;
                 }
-            } else {
-                answer = 0;
             }
-            System.out.println("#" + tc + " " + answer);
+
+            sb.append("#").append(tc).append(" ").append(ans).append("\n");
         }
+
+        System.out.println(sb);
+    }
+
+    private static String[] getCodeArray(int mul) {
+        String[] codes = new String[10];
+        for (int i = 0; i < 10; i++) {
+            String rate = rates[i];
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < 4; j++) {
+                // i가 홀수면 1, 짝수면 0
+                int num = Character.digit(rate.charAt(j), 10);
+                for (int k = 0; k < num * mul; k++) {
+                    sb.append(j % 2 == 0 ? 0 : 1);
+                }
+            }
+
+            codes[i] = sb.toString();
+        }
+
+        return codes;
     }
 }
