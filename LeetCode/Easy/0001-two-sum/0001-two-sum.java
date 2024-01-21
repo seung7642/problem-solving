b56e9a768558645896598b1f73b64cb10f4a229e
Seungho Lee <seung7642@gmail.com>
2024-01-21 15:12:53 +0900

    Time: 5 ms (58.55%), Space: 46 MB (8.07%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.java
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -1,16 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        IntStream.range(0, nums.length)
-            .forEach(idx -> {
-                if (map.containsKey(nums[idx])) {
-                    map.get(nums[idx]).add(idx);
-                } else {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(idx);
-                    map.put(nums[idx], list);
-                }
-            });
+        Map<Integer, List<Integer>> map = groupIndicesByValue(nums);
 
         int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
@@ -30,5 +20,20 @@ class Solution {
         }
 
         return result;
+    }
+    
+    private Map<Integer, List<Integer>> groupIndicesByValue(int[] nums) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        IntStream.range(0, nums.length)
+            .forEach(idx -> {
+                if (map.containsKey(nums[idx])) {
+                    map.get(nums[idx]).add(idx);
+                } else {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(idx);
+                    map.put(nums[idx], list);
+                }
+            });
+        return map;
     }
 }
