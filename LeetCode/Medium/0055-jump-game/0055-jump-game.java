496a5dba18f48c3cbb32e4457a0c27c201446a67
Seungho Lee <seung7642@gmail.com>
2024-02-08 14:53:58 +0900

    Time: 295 ms (12.66%), Space: 46.3 MB (6.63%) - LeetHub

--- LeetCode/Medium/0055-jump-game/0055-jump-game.java
+++ LeetCode/Medium/0055-jump-game/0055-jump-game.java
@@ -1,22 +1,34 @@
+enum Index {
+    GOOD, BAD, UNKNOWN
+}
+
 class Solution {
+    
+    private Index[] memo;
+    
     public boolean canJump(int[] nums) {
+        memo = new Index[nums.length];
+        for (int i = 0; i < memo.length; i++) {
+            memo[i] = Index.UNKNOWN;
+        }
+        memo[memo.length - 1] = Index.GOOD;
+        return canJumpFromPosition(0, nums);
+    }
+    
+    private boolean canJumpFromPosition(int position, int[] nums) {
+        if (memo[position] != Index.UNKNOWN) {
+            return memo[position] == Index.GOOD;
+        }
         
-        int len = nums.length;
-        boolean[] check = new boolean[len];
-        check[0] = true;
-        for (int i = 0; i < len; i++) {
-            if (!check[i]) {
-                continue;
-            }
-            
-            for (int j = 1; j <= nums[i]; j++) {
-                if (i + j >= len) {
-                    break;
-                }
-                check[i + j] = true;
+        int furthestJump = Math.min(position + nums[position], nums.length - 1);
+        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
+            if (canJumpFromPosition(nextPosition, nums)) {
+                memo[position] = Index.GOOD;
+                return true;
             }
         }
         
-        return check[len - 1];
+        memo[position] = Index.BAD;
+        return false;
     }
 }
