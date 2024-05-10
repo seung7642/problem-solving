e1f11867d71e9d8f1dc38de0a772dfadedce9183
Seungho Lee <seung7642@gmail.com>
2024-05-11 00:39:36 +0900

    Time: 0 ms (100.00%), Space: 41.9 MB (47.43%) - LeetHub

--- LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,39 +1,28 @@
 class Solution {
+    
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        int left = 0, right = n - 1;
-        
-        // Find the index of the pivot element (the smallest element)
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] > nums[n - 1]) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        // 1. 가장 작은 값의 인덱스를 찾는다. (시작 인덱스)
+        int k = 0;
+        int len = nums.length;
+        int start = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < start) {
+                start = nums[i];
+                k = i;
             }
         }
         
-        // Binary search over elements on the pivot element's left 
-        int answer = binarySearch(nums, 0, left - 1, target);
-        if (answer != -1) {
-            return answer;
-        }
-        
-        // Binary search over elements on the pivot element's right 
-        return binarySearch(nums, left, n - 1, target);
-    }
-    
-    // Binary search over an inclusive range [left_boundary ~ right_boundary]
-    private int binarySearch(int[] nums, int leftBoundary, int rightBoundary, int target) {
-        int left = leftBoundary, right = rightBoundary;
+        // 2. 가장 작은 값의 인덱스를 k라고 했을 때, 0 ~ n-1 번 인덱스를 이진 탐색하는데, 이때 인덱스에 k만큼을 더해준다. 
+        int left = 0 + k;
+        int right = nums.length - 1 + k;
         while (left <= right) {
             int mid = (left + right) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else {
+            if (nums[mid % len] == target) {
+                return mid % len;
+            } else if (nums[mid % len] < target) {
                 left = mid + 1;
+            } else if (nums[mid % len] > target) {
+                right = mid - 1;
             }
         }
         return -1;
