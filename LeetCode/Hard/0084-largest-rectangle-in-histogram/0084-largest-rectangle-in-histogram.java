5ff7eb3b5d30ecf09cdfa20c4a7138981a94e6bd
Seungho Lee <seung7642@gmail.com>
2024-05-11 23:08:12 +0900

    Time: 70 ms (54.33%), Space: 57.8 MB (21.85%) - LeetHub

--- LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,43 +1,24 @@
 class Solution {
     
     public int largestRectangleArea(int[] heights) {
-//         Deque<Integer> stack = new ArrayDeque<>();
-//         stack.push(-1);
-//         int length = heights.length;
-//         int maxArea = 0;
-        
-//         for (int i = 0; i < length; i++) {
-//             while ((stack.peek() != -1) && (heights[stack.peek()] >= heights[i])) {
-//                 int currentHeight = heights[stack.pop()];
-//                 int currentWidth = i - stack.peek() - 1;
-//                 maxArea = Math.max(maxArea, currentHeight * currentWidth);
-//             }
-//             stack.push(i);
-//         }
-        
-//         while (stack.peek() != -1) {
-//             int currentHeight = heights[stack.pop()];
-//             int currentWidth = length - stack.peek() - 1;
-//             maxArea = Math.max(maxArea, currentHeight * currentWidth);
-//         }
-//         return maxArea;
-        
         Stack<Integer> stack = new Stack<>();
-        int maxArea = 0;
-        int n = heights.length;
-
-        for (int i = 0; i <= n; i++) {
-            int h = (i == n ? 0 : heights[i]);
-            if (stack.isEmpty() || h >= heights[stack.peek()]) {
-                stack.push(i);
-            } else {
-                int tp = stack.pop();
-                int width = stack.isEmpty() ? i : i - 1 - stack.peek();
-                maxArea = Math.max(maxArea, heights[tp] * width);
-                i--;  // 현재 인덱스를 다시 처리하기 위해 감소
+        stack.push(-1);
+        int max = 0;
+        for (int i = 0; i < heights.length; i++) {
+            while (stack.peek() != -1 && heights[stack.peek()] >= heights[i]) {
+                int currentHeight = heights[stack.pop()];
+                int currentWeight = i - 1 - stack.peek();
+                max = Math.max(max, currentHeight * currentWeight);
             }
+            stack.push(i);
         }
-
-        return maxArea;
+        
+        while (stack.peek() != -1) {
+            int currentHeight = heights[stack.pop()];
+            int currentWeight = heights.length - 1 - stack.peek();
+            max = Math.max(max, currentHeight * currentWeight);
+        }
+        
+        return max;
     }
 }
