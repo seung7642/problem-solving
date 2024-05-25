27267cb9c78a218808ea65c4bcc15fad17961f82
Seungho Lee <seung7642@gmail.com>
2024-05-25 15:59:59 +0900

    Time: 4 ms (20.79%), Space: 44.6 MB (5.24%) - LeetHub

--- LeetCode/Medium/0090-subsets-ii/0090-subsets-ii.java
+++ LeetCode/Medium/0090-subsets-ii/0090-subsets-ii.java
@@ -3,23 +3,32 @@ class Solution {
     Set<List<Integer>> result = new HashSet<>();
     
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        int n = nums.length;
+        
         Arrays.sort(nums);
-        backtracking(nums, 0, new ArrayList<>());
-        return new ArrayList<>(result);
-    }
-    
-    private void backtracking(int[] nums, int depth, List<Integer> list) {
-        if (depth > nums.length) {
-            return;
+        
+        int maxNumberOfSubsets = (int) Math.pow(2, n);
+        Set<String> seen = new HashSet<>();
+        
+        for (int subsetIndex = 0; subsetIndex < maxNumberOfSubsets; subsetIndex++) {
+            List<Integer> currentSubset = new ArrayList<>();
+            StringBuilder hashcode = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                int mask = 1 << j;
+                int isSet = mask & subsetIndex;
+                if (isSet != 0) {
+                    currentSubset.add(nums[j]);
+                    hashcode.append(nums[j]).append(",");
+                }
+            }
+            
+            if (!seen.contains(hashcode.toString())) {
+                seen.add(hashcode.toString());
+                subsets.add(currentSubset);
+            }
         }
         
-        result.add(new ArrayList<>(list));
-        for (int i = depth; i < nums.length; i++) {
-            list.add(nums[i]);
-            
-            backtracking(nums, i + 1, list);
-            
-            list.remove(list.size() - 1);
-        }
+        return subsets;
     }
 }
