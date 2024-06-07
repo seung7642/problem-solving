e3fa4d9886fa7389b6fc56f0949f38ee88f03762
Seungho Lee <seung7642@gmail.com>
2024-06-07 23:39:47 +0900

    Time: 2 ms (70.77%), Space: 44.2 MB (56.83%) - LeetHub

--- LeetCode/Medium/0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ LeetCode/Medium/0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,19 +15,13 @@
  */
 class Solution {
     
-    int preorderIndex;
-    Map<Integer, Integer> inorderIndexMap;
+    int preorderIndex = 0;
+    Map<Integer, Integer> inorderIndexMap = new HashMap<>();
     
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        // pre-order 순서로 탐색된 배열을 가지고 Root 를 찾을 수 있다.
-        // in-order 순서로 탐색된 배열에서 Root를 기준으로 두 개의 SubTree (Left-Subtree, Right-Subtree) 로 나눌 수 있다.
-        
-        preorderIndex = 0;
-        inorderIndexMap = new HashMap<>();
         for (int i = 0; i < inorder.length; i++) {
             inorderIndexMap.put(inorder[i], i);
-        }        
-        
+        }
         return arrayToTree(preorder, 0, preorder.length - 1);
     }
     
@@ -35,13 +29,10 @@ class Solution {
         if (left > right) {
             return null;
         }
-
-        // select the preorder_index element as the root and increment it
+        
         int rootValue = preorder[preorderIndex++];
         TreeNode root = new TreeNode(rootValue);
         
-        // build left and right subtree
-        // excluding inorderIndexMap[rootValue] element because it's the root
         root.left = arrayToTree(preorder, left, inorderIndexMap.get(rootValue) - 1);
         root.right = arrayToTree(preorder, inorderIndexMap.get(rootValue) + 1, right);
         return root;
