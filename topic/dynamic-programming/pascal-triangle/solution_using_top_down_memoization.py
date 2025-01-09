ac194d85f285d6faf1df32770e0917ce9c516541
seung7642 <seung7642@gmail.com>
2025-01-09 09:37:38 +0900

    fix: Correct invalid Top-Down Memoization DP logic

--- topic/dynamic-programming/pascal-triangle/solution_using_top_down_memoization.py
+++ topic/dynamic-programming/pascal-triangle/solution_using_top_down_memoization.py
@@ -1,23 +1,14 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        result = [[None] * (i + 1) for i in range(numRows)]
+        memo = {}
+        def getValue(row, col) -> int:
+            if row == col or col == 0:
+                return 1
 
-        def getPascalByRow(row) -> List[int]:
-            if row == 0:
-                result[row][0] = 1
-                return result[row][0]
+            if (row, col) in memo:
+                return memo[(row, col)]
 
-            if result[row][0] is not None:
-                return result[row]
-
-            previous_row = getPascalByRow(row - 1)
-            result[row][0] = 1
-            result[row][row] = 1
-            for i in range(1, row):
-                result[row][i] = previous_row[i - 1] + previous_row[i]
-
-            return result[row]
-
-        getPascalByRow(numRows - 1)
-        return result
-    
+            memo[(row, col)] = getValue(row - 1, col - 1) + getValue(row - 1, col)
+            return memo[(row, col)]
+                
+        return [[getValue(i, j) for j in range(i + 1)] for i in range(numRows)]
