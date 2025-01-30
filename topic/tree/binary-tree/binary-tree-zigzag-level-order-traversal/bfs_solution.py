6c04c5e991e8ae29a6b5dd11e7a08c4338a756b1
이승호 <seung7642@gmail.com>
2025-01-30 17:07:15 +0900

    feat: A solution using the BFS algorithm

--- topic/tree/binary-tree/binary-tree-zigzag-level-order-traversal/bfs_solution.py
+++ topic/tree/binary-tree/binary-tree-zigzag-level-order-traversal/bfs_solution.py
@@ -14,33 +14,27 @@ class Solution:
 
         result = []
         level_list = deque()
-
-        # start with the level 0 with a delimiter
         node_queue = deque([root, None])
         is_left_to_right = True
-
         while node_queue:
-            current_node = node_queue.popleft()
+            cur_node = node_queue.popleft()
 
-            if current_node:
-                if is_left_to_right:
-                    level_list.append(current_node.val)
-                else:
-                    level_list.appendleft(current_node.val)
-
-                for child in (current_node.left, current_node.right):
-                    if child:
-                        node_queue.append(child)
-            else:
-                # we finish one level
-                result.append(list(level_list))
-
+            if cur_node is None:
                 if node_queue:
-                    # add a delimiter to mark the level
                     node_queue.append(None)
 
-                    # prepare for the next level
-                    level_list = deque()
-                    is_left_to_right = not is_left_to_right
+                result.append(list(level_list))
+                level_list = deque()
+                is_left_to_right = not is_left_to_right
+                continue
+            
+            if is_left_to_right:
+                level_list.append(cur_node.val)
+            else:
+                level_list.appendleft(cur_node.val)
+            
+            for child in (cur_node.left, cur_node.right):
+                if child:
+                    node_queue.append(child)
 
         return result
